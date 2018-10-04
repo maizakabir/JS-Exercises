1fa37930e7cd6a75d9c452de1f7cfad16af575fb
Amirah Kabir <maiza1497@gmail.com>
2018-10-04 03:16:50 +0000

    Task 2

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 //Task 1
 
 console.log("External JavaScript");
+
+//Task 2
+function sumArray (arr){
+    var sum=0;
+    arr.forEach (function (value, index){
+        sum = sum + value;
+    });
+    return sum;
+}
+
+console.log (sumArray([2, 4, 5, 6, 7]));
