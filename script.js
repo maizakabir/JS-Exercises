c6bd2d187929ef6ff69e780e3549c24b78bb365a
Amirah Kabir <maiza1497@gmail.com>
2018-10-08 02:53:52 +0000

    Task 4 completed

--- script.js
+++ script.js
@@ -26,3 +26,31 @@ console.log(checkEmail("john@smith.com"));
 console.log(checkEmail("john6@sh.com"));
 console.log(checkEmail("john@smith"));
 console.log(checkEmail("johnsmith.com"));
+
+//Task 4
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+function getReadingStatus (index){
+    var book= library [index];
+    return book.readingStatus;
+    //return library [index].readingStatus; (same as the above)
+}
+
+console.log(getReadingStatus(1));
+console.log(getReadingStatus(0));
+console.log(getReadingStatus(2));
